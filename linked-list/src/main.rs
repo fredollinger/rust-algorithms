523ad4e11623584c26c13e9a0f8fdf8d465da933
Frederick Ollinger <follinge@gmail.com>
2018-01-14 10:51:29 -0800

    Fix LinkedList
    Now our linked list will have a single member, head, which is a Node.
    
    The Node head will point to another Node, z, which is created in
    LinkedList::new().
    
    This should allow us create and point to new Nodes.

--- linked-list/src/main.rs
+++ linked-list/src/main.rs
@@ -8,7 +8,7 @@ impl Node {
        let z: Box<Node> = Box::new(Node {
             data: 0,
             node: None
-        });
+       });
 
         let head = Node {
             data: 0,
@@ -20,35 +20,34 @@ impl Node {
 
 struct LinkedList {
     junk: i32, // remove me
-    head: Box<Node>,
-    z: Box<Node>
+    head: Box<Node>
+    //z: Box<Node>
 }
 
 impl LinkedList {
     pub fn new() -> LinkedList {
+        let z: Box<Node> = Box::new(Node {
+            data: 0,
+            node: None
+        });
+
         let ll = LinkedList {
             junk: 4,
-            z: Box::new(Node {
-                data: 4,
-                node: None
-            }),
-
-            // https://stackoverflow.com/questions/32300132/why-cant-i-store-a-value-and-a-reference-to-that-value-in-the-same-struct
             head: Box::new(Node {
-                data: 0,
-                //node: Some(z)
-                node: None
+                data: 4,
+                node: Some(z)
             })
         };
         ll
     } // END LinkedList new()
 
-    pub fn init(ll: &mut LinkedList) {
+    pub fn print(ll: LinkedList) {
+        println!("{}", ll.head.data);
         // ll.head.node = Some(ll.z);
     }
 } // END impl LinkedList
 
 fn main() {
     let mut _ll = LinkedList::new();
-    LinkedList::init(&mut _ll);
+    LinkedList::print(_ll);
 }
