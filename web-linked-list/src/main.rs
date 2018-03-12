27313a04d67695899d58cce2d92b3998472feafb
Frederick Ollinger <follinge@gmail.com>
2018-03-12 07:48:03 -0700

    Initial broken list

---
+++ web-linked-list/src/main.rs
@@ -0,0 +1,15 @@
+impl List {
+    pub fn new() -> Self {
+        List { head: Link::Empty }
+    }
+}
+
+/*
+fn foo(arg1: Type1, arg2: Type2) -> ReturnType {
+        // body
+}
+*/
+
+fn main() {
+    println!("Hello, world!");
+}
