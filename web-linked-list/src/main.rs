8d12af69b2967308adc0c1990988cb05909e08cc
Frederick Ollinger <fred.ollinger@seescan.com>
2018-04-13 09:59:14 -0700

    Add bad linked list

--- web-linked-list/src/main.rs
+++ web-linked-list/src/main.rs
@@ -1,8 +1,14 @@
+pub enum List {
+        Empty,
+            Elem(i32, Box<List>),
+}
+/*
 impl List {
     pub fn new() -> Self {
         List { head: Link::Empty }
     }
 }
+*/
 
 /*
 fn foo(arg1: Type1, arg2: Type2) -> ReturnType {
