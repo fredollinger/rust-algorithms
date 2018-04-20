4c4bae06afabf1511474905b45e3bbcae7246cde
Frederick Ollinger <fred.ollinger@seescan.com>
2018-04-20 11:22:02 -0700

    Usable linked list

--- web-linked-list/src/main.rs
+++ web-linked-list/src/main.rs
@@ -1,7 +1,17 @@
-pub enum List {
-        Empty,
-            Elem(i32, Box<List>),
+pub struct List {
+    head: Link,
 }
+
+enum Link {
+    Empty,
+    More(Box<Node>),
+}
+
+struct Node {
+    elem: i32,
+    next: List,
+}
+
 /*
 impl List {
     pub fn new() -> Self {
