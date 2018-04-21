26af129f92bc3b6ed501e7d87ddd621f88ad9546
Frederick Ollinger <follinge@gmail.com>
2018-04-20 18:37:35 -0700

    Add basic list layout

--- web-linked-list/src/main.rs
+++ web-linked-list/src/main.rs
@@ -1,14 +1,22 @@
+pub struct List {
+    head: Link,
+}
+
+enum Link {
+    Empty,
+    More(Box<Node>),
+}
+
+struct Node {
+    elem: i32,
+    next: Link,
+}
+
 impl List {
     pub fn new() -> Self {
         List { head: Link::Empty }
     }
 }
-
-/*
-fn foo(arg1: Type1, arg2: Type2) -> ReturnType {
-        // body
-}
-*/
 
 fn main() {
     println!("Hello, world!");
