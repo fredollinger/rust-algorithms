b0f4f16ff25f849b31d408174a6d60b700fedd64
Frederick Ollinger <fred.ollinger@seescan.com>
2018-04-23 11:20:35 -0700

    Add broken push()

--- web-linked-list/src/main.rs
+++ web-linked-list/src/main.rs
@@ -1,3 +1,5 @@
+use std::mem;
+
 pub struct List {
     head: Link,
 }
@@ -12,19 +14,18 @@ struct Node {
     next: List,
 }
 
-/*
 impl List {
-    pub fn new() -> Self {
-        List { head: Link::Empty }
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem: elem,
+            //next: List(self.head),
+            // help: try using a variant of the expected type: `List(Link::Empty)`
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
+    
+        // self.head = Link::More(new_node);
     }
 }
-*/
-
-/*
-fn foo(arg1: Type1, arg2: Type2) -> ReturnType {
-        // body
-}
-*/
 
 fn main() {
     println!("Hello, world!");
